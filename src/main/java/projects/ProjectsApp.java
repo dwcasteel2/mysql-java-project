ed9d7379daab4605c1e7bf582ddab633945601d5
dwcasteel2 <dwcasteel2@gmail.com>
2024-02-03 22:13:22 -0800

    week 10

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -12,41 +12,70 @@ public class ProjectsApp {
 
 	private Scanner scanner = new Scanner(System.in);
 	private ProjectService projectService = new ProjectService();
-		//@formatter:off
+	private Project curProject;
+	//@formatter:off
 		private List<String> operations = List.of(
-				"1) Add a project"
+				"1) Add a project",
+				"2) List projects",
+				"3) Select a project"
 				);
+
 		//@formatter:on
-		
+
 	public static void main(String[] args) {
-		new ProjectsApp().processUserSelections();			
-		}
+		new ProjectsApp().processUserSelections();
+	}
 
 	private void processUserSelections() {
 		boolean done = false;
-		
-		while(!done) {
+
+		while (!done) {
 			try {
 				int selection = getUserSelections();
-				
-				switch(selection) {
+
+				switch (selection) {
 				case -1:
 					done = exitMenu();
 					break;
-				
+
 				case 1:
 					createProject();
 					break;
-									
+
+				case 2:
+					listProjects();
+					break;
+					
+				case 3:
+					selectProject();
+					break;
+
 				default:
 					System.out.println("\n" + selection + " is not a valid selection. Try again.");
 					break;
 				}
-			}
-			catch(Exception e) {
+			} catch (Exception e) {
 				System.out.println("\nError: " + e + " Try again.");
 			}
 		}
+	}
+
+	private void selectProject() {
+		listProjects();
+		Integer projectId = getIntInput("Enter a prject ID to select a project");
+		
+		curProject = null;
+		
+		curProject = projectService.fetchProjectById(projectId);
+	}
+	
+	private void listProjects() {
+		List<Project> projects = projectService.fetchAllProjects();
+
+		System.out.println("\nProjects:");
+
+		projects.forEach(
+				project -> System.out.println("   " + project.getProjectId() + ": " + project.getProjectName()));
 	}
 
 	private void createProject() {
@@ -55,31 +84,30 @@ public class ProjectsApp {
 		BigDecimal actualHours = getDecimalInput("Enter actual hours");
 		Integer difficuly = getIntInput("Enter the project difficuly (1-5)");
 		String notes = getStringInput("Enter project notes");
-		
+
 		Project project = new Project();
-		
+
 		project.setProjectName(projectName);
 		project.setEstimatedHours(estimatedHours);
 		project.setActualHours(actualHours);
 		project.setDifficulty(difficuly);
 		project.setNotes(notes);
-		
+
 		Project dbProject = projectService.addProject(project);
 		System.out.println("You have successfully created project: " + dbProject);
 	}
 
 	private BigDecimal getDecimalInput(String prompt) {
 		String input = getStringInput(prompt);
-		
-			if(Objects.isNull(input)) {
-				return null;
+
+		if (Objects.isNull(input)) {
+			return null;
 		}
-		
-			try {
-				return new BigDecimal(input).setScale(2);
-		}
-			catch(NumberFormatException e) {
-				throw new DbException(input + " is not a valid decimal number");
+
+		try {
+			return new BigDecimal(input).setScale(2);
+		} catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid decimal number");
 		}
 	}
 
@@ -91,37 +119,42 @@ public class ProjectsApp {
 	private int getUserSelections() {
 		printOperations();
 		Integer input = getIntInput("Enter a meun selection");
-		
+
 		return Objects.isNull(input) ? -1 : input;
 	}
 
 	private Integer getIntInput(String prompt) {
 		String input = getStringInput(prompt);
-		
-		if(Objects.isNull(input)) {
+
+		if (Objects.isNull(input)) {
 			return null;
 		}
-		
+
 		try {
 			return Integer.valueOf(input);
-		}
-		catch(NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			throw new DbException(input + " is not a valid number");
 		}
-		
+
 	}
 
 	private String getStringInput(String prompt) {
 		System.out.println(prompt + ": ");
 		String input = scanner.nextLine();
-		
+
 		return input.isBlank() ? null : input.trim();
-		}
+	}
+
 	private void printOperations() {
 		System.out.println("\nThese are the available selections. Press the Enter key to quit:");
-	
-	operations.forEach(line -> System.out.println(" " + line));
-}
-}
 
-
+		operations.forEach(line -> System.out.println(" " + line));
+		
+		if(Objects.isNull(curProject)) {
+			System.out.println("\nYou are not working with a project.");
+		}
+		else {
+			System.out.println("\nYou are working with project: " + curProject);
+		}
+	}
+}
